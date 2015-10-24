5a172d399f57fbbe700f382a251edb04434a8dc8
Aidan Skinner <aidan@skinner.me.uk>
2015-10-24 18:51:14 +0100

    fix step insertion

--- behave/super_cache.py
+++ behave/super_cache.py
@@ -25,14 +25,13 @@ def super_cache_parser(features):
     for index, runs in step_counts.items():
         if len(runs) > 1:
             # Add a cache step to the first scenario
-            cache_step = Step("", "", "When", "When", "the system state is cached to %s" % index)
+            cache_step = Step(u"", u"", u"when", u"when", u"the system state is cached to %s" % index)
             first_scenario, step_index = runs.pop(0)
 
-            first_scenario.insert(step_index, cache_step)
+            first_scenario.steps.insert(step_index, cache_step)
 
             # Replace the tree in subsequent scenarios up to this point with a cache restore
-            restore_step = Step("", "", "When", "When", "the system state is restored from cache %s" % index)
-            for other_scenario in runs:
-                other_scenario.steps = [restore_step] + other_scenario.steps[step_index:]
-
+            restore_step = Step(u"", u"", u"when", u"when", u"the system state is restored from cache %s" % index)
+            for other_scenario, step_index in runs:
+                other_scenario.steps = [restore_step] + other_scenario.steps[step_index+1:]
     return features
